12bfdd798c8dfca14753c8d95d630283ec15d913
Astrea49 <25420078+Astrea49@users.noreply.github.com>
2023-01-02 18:50:11 -0500

    fix: properly import naff speedups

--- tansy/__init__.py
+++ tansy/__init__.py
@@ -10,7 +10,7 @@ __version__ = "0.5.0"
 
 from .slash_commands import *
 from .slash_param import *
-from .speedup.install import install_naff_speedups
+from .speedup import install_naff_speedups
 
 __all__ = (
     "TansySlashCommandParameter",
---
+++ tansy/speedup/__init__.py
@@ -0,0 +1,3 @@
+from .install import install_naff_speedups
+
+__all__ = ("install_naff_speedups",)
